0e70d3e89e0861aa9d9683e3ff2d0bc808eae7e4
joanbosch <joanbosch@users.noreply.github.com>
2022-10-24 21:46:52 +0000

    Deploying to gh-pages from @ joanbosch/joanbosch.github.io@554a5ac9702e76eea66c508c8298a72b1d954a68 🚀

---
+++ _next/static/I7COb7QLRvdbf7yW-2Vc-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/970-3e77d7348d3f5df4.js","static/chunks/pages/index-e98273b3be882595.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/RnAwsWnFaSckGOBtWGTKE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/970-3e77d7348d3f5df4.js","static/chunks/pages/index-2c8c824bb6c7497e.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
