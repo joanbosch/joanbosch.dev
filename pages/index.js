8239da65731f3f6d1f8b92377724c70df1584716
Joan Bosch <jbosch@adiquimica.com>
2022-10-24 23:25:32 +0200

    Add Avatar component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import PageLayout from '../components/PageLayout'
+import Avatar from '../components/Avatar'
+import { useTranslation } from './../utils/locales';
 
 export default function Home() {
+  const { hero } = useTranslation();
   return (
     <PageLayout>
+      <Avatar locales={hero}/>
     </PageLayout>
   )
 }
