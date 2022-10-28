7f52a6e812faf92c600062991697c121881a7143
Joan Bosch <jbosch@adiquimica.com>
2022-10-29 00:26:09 +0200

    Save commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import PageLayout from '../components/PageLayout'
 import Avatar from '../components/Avatar'
 import Services from '../components/Services';
+import Stack from '../components/Stack';
 import { useTranslation } from './../utils/locales';
 
 export default function Home() {
-  const { hero, services } = useTranslation();
+  const { hero, services, stack } = useTranslation();
   return (
     <PageLayout>
       <Avatar locales={hero}/>
       <Services locales={services}/>
+      <Stack locales={stack}/>
     </PageLayout>
   )
 }
